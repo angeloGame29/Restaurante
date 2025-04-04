0dd5432c39a4e4c4c5a3ab13b78a612240fe9149
Diogo <diogo.silva@dcx.ufpbr.br>
2025-04-04 19:21:42 -0300

    Fiz os metodos com HashMap -Diogo

--- src/main/java/br/ufpb/dcx/restaurante/MeuSistemaRestaurante.java
+++ src/main/java/br/ufpb/dcx/restaurante/MeuSistemaRestaurante.java
@@ -4,113 +4,91 @@ import br.ufpb.dcx.restaurante.classDeException.PedidoExistenteException;
 import br.ufpb.dcx.restaurante.classDeException.PedidoInexistenteException;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class MeuSistemaRestaurante implements SistemaRestaurante {
-    private List<Pedido> pedidos;
-    private final GravadorPedidosRestaurante gravadorPedidos;
+    private Map<String, Pedido> pedidoMap;
 
-    public MeuSistemaRestaurante() {
-        this.pedidos = new ArrayList<>();
-        this.gravadorPedidos = new GravadorPedidosRestaurante();
+    public MeuSistemaRestaurante(){
+        this.pedidoMap = new HashMap<>();
     }
 
-    public boolean existePedidoCodigo(String codigo){
-        for(Pedido tarefa: this.pedidos){
-            if (tarefa.getCodPedido().equals(codigo)){
-                return true;
-            }
+    @Override
+    public void cadastrarPedido(String codigo, String nomeCLiente, String numMesa
+            , List<ItemPedido> itens, SistemaData dataPedido, String statusPedido) throws PedidoExistenteException {
+        if(pedidoMap.containsKey(codigo)){
+            throw new PedidoExistenteException("Já existe pedido com esse código: "+ codigo);
+        } else {
+            Pedido pedido = new Pedido(codigo, nomeCLiente, numMesa, itens, dataPedido, statusPedido);
         }
-        return false;
     }
 
-    public void cadastrarPedido(String nomeCliente, String codPedido, String numMesa, List<ItemPedido> itens, SistemaData dataPedido, String statusPedido)
-            throws PedidoExistenteException{
-        if (existePedidoCodigo(codPedido)){
-            throw new PedidoExistenteException("Já existe uma tarefa com a descrição "+ codPedido);
+    @Override
+    public boolean existePedidoCodigo(String codigo) {
+        return pedidoMap.containsKey(codigo);
+    }
+
+    @Override
+    public List<Pedido> pesquisarPedidosDoDia(int dia, int mes, int ano) throws PedidoInexistenteException {
+        List<Pedido> pedidosDoDia = this.pedidoMap.values().stream()
+                .filter(pedido -> Objects.equals(pedido.getDataPedido(),
+                new SistemaData(dia, mes, ano))).toList();
+        if(pedidosDoDia.isEmpty()){
+            throw new PedidoInexistenteException("Não existe pedido nessa data: "+ dia +"/"+ mes+"/"+ ano);
+        } else {
+            return pedidosDoDia;
         }
-        Pedido pedido = new Pedido(nomeCliente,codPedido, numMesa, itens,dataPedido, statusPedido);
-        this.pedidos.add(pedido);
     }
 
-    public void cadastrarPedido(Pedido pedido) throws PedidoExistenteException {
-        if (this.existePedidoCodigo(pedido.getCodPedido())){
-            throw new PedidoExistenteException("Já existe uma tarefa com a descrição "+
-                    pedido.getCodPedido());
-        }
-        this.pedidos.add(pedido);
-    }
-
-    public List<Pedido> pesquisarPedidosDoDia(int dia,int mes,int ano) {
-        List<Pedido> pedidosDoDia = new ArrayList<>();
-        for (Pedido pedido : this.pedidos) {
-            if (pedido.getDataPedido().getDia()==dia && pedido.getDataPedido().getMes()==mes
-                    && pedido.getDataPedido().getAno()==ano){
-                pedidosDoDia.add(pedido);
-                return pedidosDoDia;
-            }
-        }
-        return null;
-
-    }
-
+    @Override
     public Pedido PesquisaPedidoPorCodigo(String codigo) throws PedidoInexistenteException {
-        for (Pedido pedido : this.pedidos) {
-            if (pedido.getCodPedido().equals(codigo)) {
-                return pedido;
-            }
-        }
-        throw new PedidoInexistenteException("Pedido com código " + codigo + " não encontrado.");
-
+        Optional<Pedido> pedidoPesquisado = this.pedidoMap.values().stream()
+                .filter(pedido -> pedidoMap.containsKey(codigo)).findFirst();
+        return pedidoPesquisado.orElseThrow(() -> new PedidoInexistenteException
+                ("Não existe pedido com o código: "+ codigo));
     }
 
-    public void adicionarAoPedido(String codPedido, List<ItemPedido> novoItemPedido) throws PedidoInexistenteException {
-        for (Pedido pedido : this.pedidos) {
-            if (pedido.getCodPedido().equals(codPedido)) {
-                pedido.getItens().addAll(novoItemPedido);
-            }
-        }
-        if(!existePedidoCodigo(codPedido)){
-            throw new PedidoInexistenteException("Não existe nenhum pedido com esse código!");
+    @Override
+    public void adicionarAoPedido(String codigo, List<ItemPedido> novoItemPedido) throws PedidoInexistenteException {
+        if(this.pedidoMap.containsKey(codigo)){
+            pedidoMap.get(codigo).getItens().addAll(novoItemPedido);
+        } else {
+            throw new PedidoInexistenteException("Não existe pedido com o código: "+ codigo);
         }
     }
 
+    @Override
     public void atualizaStatusDoPedido(String codigo, String status) throws PedidoInexistenteException {
-        for (Pedido p:this.pedidos){
-            if(p.getCodPedido().equals(codigo)){
-                p.setStatusPedido(status);
-            }
+        Pedido pedido = pedidoMap.get(codigo);
+        if (pedido == null) {
+            throw new PedidoInexistenteException("Não existe pedido com o código: " + codigo);
         }
+        pedido.setStatusPedido(status);
     }
 
-    public void cancelarPedido(String codPedido) throws PedidoInexistenteException {
-        for (int i = 0; i < this.pedidos.size(); i++){
-            Pedido p = this.pedidos.get(i);
-            if (p.getCodPedido().equals(codPedido)){
-                this.pedidos.remove(p);
-                break;
-            }
-            if (!existePedidoCodigo(codPedido)){
-                throw new PedidoInexistenteException("Não existe nenhum pedido com esse código!");
-            }
-        }
+    @Override
+    public void cancelarPedido(String codigo) throws PedidoInexistenteException {
+        this.pedidoMap.values().stream().filter(pedido -> pedido.getCodPedido().equals(codigo))
+                .findFirst().map(pedido -> this.pedidoMap.remove(codigo))
+                .orElseThrow(() -> new PedidoInexistenteException("Não existe pedido com o código: " + codigo));
     }
 
-    public double calcularTotalPedido(String nomeCliente, String codPedido) throws PedidoInexistenteException {
-        for (Pedido p : pedidos) {
-            if (p.getNomeCliente().equalsIgnoreCase(nomeCliente) && p.getCodPedido().equalsIgnoreCase(codPedido)) {
-                return p.calcularTotal();
+    @Override
+    public double calcularTotalPedido(String codigo) throws PedidoInexistenteException {
+        for (Pedido pedido: this.pedidoMap.values()){
+            if (this.pedidoMap.containsKey(codigo)){
+                return pedido.calcularTotal();
             }
-        }
-        throw new PedidoInexistenteException("Pedido para o cliente " + nomeCliente + " não encontrado.");
+        } throw new PedidoInexistenteException("Não existe pedido com o código: " + codigo);
     }
 
+    @Override
     public void salvarPedido() throws IOException {
-        this.gravadorPedidos.gravarPedidos(this.pedidos);
+
     }
 
+    @Override
     public void recuperarPedido() throws IOException {
-        this.pedidos = this.gravadorPedidos.recuperaPedidos();
+
     }
 }
--- src/main/java/br/ufpb/dcx/restaurante/SistemaRestaurante.java
+++ src/main/java/br/ufpb/dcx/restaurante/SistemaRestaurante.java
@@ -6,38 +6,32 @@ import java.io.IOException;
 import java.util.List;
 public interface SistemaRestaurante {
     /**
-     * Confere se um pedido existe ou não
-     * @param codigo recebe o codigo do pedido para saber se existe ou não
-     * @return true para pedido existente e false para pedido não existente
-     */
-    public boolean existePedidoCodigo(String codigo);
-
-    /**
      * Cadastra um pedido com seus dados
      * @param nomeCLiente recebe o nome do cliente
-     * @param codPedido recebe um codigo
+     * @param codigo recebe um codigo
      * @param numMesa recebe o num da mesa em que o cliente está
      * @param itens recebe os itens lanche/bebida
      * @param dataPedido recebe o data que foi feito o pedido
      * @param statusPedido recebe o status em que se encontra aquele pedido
      * @throws PedidoExistenteException
      */
-    void cadastrarPedido(String nomeCLiente, String codPedido, String numMesa,List<ItemPedido> itens, SistemaData dataPedido, String statusPedido)
-            throws PedidoExistenteException;
+    void cadastrarPedido( String codigo, String nomeCLiente, String numMesa,List<ItemPedido> itens, SistemaData dataPedido, String statusPedido)
+    throws PedidoExistenteException;
 
     /**
-     * cadastra um pedido
-     * @param pedido recebe um pedido para ser cadastrado
-     * @throws PedidoExistenteException
+     * Confere se um pedido existe ou não
+     * @param codigo recebe o codigo do pedido para saber se existe ou não
+     * @return true para pedido existente e false para pedido não existente
      */
-    void cadastrarPedido(Pedido pedido) throws PedidoExistenteException;
+    boolean existePedidoCodigo(String codigo);
 
     /**
-     * Pesquisar todos os pedidos do dia
-     * @param dia o dia a qual será pesquisado
+     * Pesquisar todos os pedidos do mes
+     * @param mes o dia a qual será pesquisado
+     * @param ano indica o ano do mes
      * @return retorna uma lista com todos os pedidos feitos no dia
      */
-    List<Pedido> pesquisarPedidosDoDia(int dia, int mes,int ano);
+    List<Pedido> pesquisarPedidosDoDia(int dia, int mes, int ano) throws PedidoInexistenteException;
 
     /**
      * Pesquisa um pedido em específico
@@ -50,11 +44,11 @@ public interface SistemaRestaurante {
 
     /**
      * Atualiza os itens pedidos pelo cliente
-     * @param codPedido o codigo do pedido do cliente que terá os itens atualizados
+     * @param codigo o codigo do pedido do cliente que terá os itens atualizados
      * @param novoItemPedido os novos itens que seram atualizados no pedido
      * @throws PedidoInexistenteException
      */
-    void adicionarAoPedido(String codPedido, List<ItemPedido>novoItemPedido)
+    void adicionarAoPedido(String codigo, List<ItemPedido>novoItemPedido)
             throws PedidoInexistenteException;
 
     /**
@@ -67,20 +61,19 @@ public interface SistemaRestaurante {
 
     /**
      * Cancela um pedido com o seu codigo informado.
-     * @param codPedido o codigo do pedido que será cancelado
+     * @param codigo o codigo do pedido que será cancelado
      * @throws PedidoInexistenteException
      */
-    void cancelarPedido(String codPedido)
+    void cancelarPedido(String codigo)
             throws PedidoInexistenteException;
-    //Só retirar o pedido da lista
 
     /**
      * Calcula o valor total do pedido de um determinado cliente.
-     * @param nomeCliente o nome do cliente cujo o pedido será calculado
+     * @param codigo o codigo do cliente cujo o pedido será calculado
      * @return retornar um valor double que é o calculo da soma dos pedidos/itens
      * @throws PedidoInexistenteException
      */
-    double calcularTotalPedido(String nomeCliente, String codPedido) throws PedidoInexistenteException;
+    double calcularTotalPedido(String codigo) throws PedidoInexistenteException;
 
     void salvarPedido() throws IOException;
 
